dac1d0004ce5aba9b80e08789338a0fa978782fc
furuhama <furuhama@users.noreply.github.com>
2020-06-09 22:07:51 +0900

    Fix dfs

--- src/bin/dfs.rs
+++ src/bin/dfs.rs
@@ -1,5 +1,3 @@
-use proconio::input;
-
 #[allow(unused_macros)]
 macro_rules! echo {
     ($($e:expr),+) => ( { $(println!("{}", $e))+ } );
@@ -22,11 +20,10 @@ fn dfs(i: usize, sum: usize, a: &mut Vec<usize>, k: usize, n: usize) -> bool {
 }
 
 fn main() {
-    input! {
-        n: usize,
-        mut a: [usize; n],
-        k: usize,
-    }
+    // input
+    let n = 4;
+    let mut a = vec![1, 2, 4, 7];
+    let k = 13;
 
     if dfs(0, 0, &mut a, k, n) {
         echo!("Yes");
